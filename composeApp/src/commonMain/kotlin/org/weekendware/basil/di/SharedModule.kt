e024d42fe84e09d0ccdf865a43a066f86ba449c4
Gavin Dunne <weekendwareinbox@gmail.com>
2026-05-05 23:37:43 -0400

    Add profile screen with BG target editing (#35)
    Full profile screen: account info (name, email) read-only display with
    initials avatar, editable target BG range persisted via PreferencesRepository.
    Adds GetUserUseCase, GetBgTargetsUseCase, SetBgTargetsUseCase, and
    ProfileViewModelTest (13 tests, all green).

--- composeApp/src/commonMain/kotlin/org/weekendware/basil/di/SharedModule.kt
+++ composeApp/src/commonMain/kotlin/org/weekendware/basil/di/SharedModule.kt
@@ -13,11 +13,14 @@ import org.weekendware.basil.data.repository.SqlDelightUserRepository
 import org.weekendware.basil.data.repository.SupabaseAuthRepository
 import org.weekendware.basil.data.repository.UserRepository
 import org.weekendware.basil.domain.usecase.DeleteLogEntryUseCase
+import org.weekendware.basil.domain.usecase.GetBgTargetsUseCase
 import org.weekendware.basil.domain.usecase.GetBgUnitPreferenceUseCase
 import org.weekendware.basil.domain.usecase.GetLastBgReadingUseCase
 import org.weekendware.basil.domain.usecase.GetTodayEntriesUseCase
+import org.weekendware.basil.domain.usecase.GetUserUseCase
 import org.weekendware.basil.domain.usecase.ObserveRecentLogsUseCase
 import org.weekendware.basil.domain.usecase.SaveLogEntryUseCase
+import org.weekendware.basil.domain.usecase.SetBgTargetsUseCase
 import org.weekendware.basil.domain.usecase.SetBgUnitPreferenceUseCase
 import org.weekendware.basil.presentation.auth.AuthViewModel
 import org.weekendware.basil.presentation.chat.ChatViewModel
@@ -56,6 +59,9 @@ val useCaseModule = module {
     single { DeleteLogEntryUseCase(get()) }
     single { GetBgUnitPreferenceUseCase(get()) }
     single { SetBgUnitPreferenceUseCase(get()) }
+    single { GetUserUseCase(get()) }
+    single { GetBgTargetsUseCase(get()) }
+    single { SetBgTargetsUseCase(get()) }
 }
 
 /**
@@ -65,7 +71,7 @@ val sharedModule = module {
     viewModel { SessionViewModel(get()) }
     viewModel { AuthViewModel(get()) }
     viewModel { DashboardViewModel(get(), get(), get()) }
-    viewModel { ProfileViewModel() }
+    viewModel { ProfileViewModel(get(), get(), get()) }
     viewModel { ChatViewModel() }
     viewModel { SettingsViewModel() }
     viewModel { LoggingViewModel(get(), get(), get()) }
